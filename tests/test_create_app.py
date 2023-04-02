f6e9da5df0943ebd0026a7269e0de1c010060afb
honza <hai.linh.ng91@gmail.com>
2023-04-02 18:47:29 +0200

    Uncomment unfinished (broken) tests

--- tests/test_create_app.py
+++ tests/test_create_app.py
@@ -1,25 +1,25 @@
-from thingies import shell_command
-from pyfzf import FzfPrompt
+# from thingies import shell_command
+# from pyfzf import FzfPrompt
 
 
 
-def select_choices():
-    pass
+# def select_choices():
+#     pass
 
-INPUTS = {
-    "Nothing": select_choices
-    # "number": ,
-    # "text": 
-}
-BASE_CHOICES = {
-    "processes": lambda: shell_command("ps aux"),
-    "brotab windows": lambda: shell_command("brotab list windows")
-}
+# INPUTS = {
+#     "Nothing": select_choices
+#     # "number": ,
+#     # "text": 
+# }
+# BASE_CHOICES = {
+#     "processes": lambda: shell_command("ps aux"),
+#     "brotab windows": lambda: shell_command("brotab list windows")
+# }
 
-def test_construct_app():
-    inp = FzfPrompt().prompt(choices=INPUTS.keys())[0]
-    INPUTS[inp]()
+# def test_construct_app():
+#     inp = FzfPrompt().prompt(choices=INPUTS.keys())[0]
+#     INPUTS[inp]()
 
 
-if __name__ == "__main__":
-    test_construct_app()
+# if __name__ == "__main__":
+#     test_construct_app()
