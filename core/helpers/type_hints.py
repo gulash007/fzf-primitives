859e4771f18ff332b52e56cfc4614b9083197d41
honza <hai.linh.ng91@gmail.com>
2023-04-07 16:12:23 +0200

    type hints: Remove self argument as modules will just import prompt and use it as a global variable, prompts shouldn't return Prompt

--- core/helpers/type_hints.py
+++ core/helpers/type_hints.py
@@ -1,14 +1,15 @@
-from typing import Protocol, TypeVar, Generic, ParamSpec
+from typing import Protocol, Generic, ParamSpec
 
 from ..options import Options
-from ..Prompt import Prompt
 from ..MyFzfPrompt import Result
 
+
+__all__ = ["Moddable", "P"]
+
 P = ParamSpec("P")
-R = TypeVar("R", bound=Result | Prompt, covariant=True)
 
 
-class ModdableMethod(Protocol, Generic[P, R]):
+class Moddable(Protocol, Generic[P]):
     @staticmethod
-    def __call__(self: Prompt, options: Options, *args: P.args, **kwargs: P.kwargs) -> R:
+    def __call__(options: Options, *args: P.args, **kwargs: P.kwargs) -> Result:
         ...
