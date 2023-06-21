5676bb8e4a6d3ae732c2017a51541e53a5564e53
honza <hai.linh.ng91@gmail.com>
2023-06-22 01:43:27 +0200

    BasicLoop: Clean up, improve logging

--- core/BasicLoop.py
+++ core/BasicLoop.py
@@ -1,8 +1,11 @@
 from typing import Any, Callable
 
+from .monitoring import Logger
 from .FzfPrompt.exceptions import ExitLoop, ExitRound
 from .FzfPrompt.Prompt import Result
 from thingies import color
+
+logger = Logger.get_logger()
 
 
 class UnexpectedResultType(Exception):
@@ -21,29 +24,8 @@ class BasicLoop:
             except ExitRound:
                 continue
             except ExitLoop as e:
-                print(f"{color('Exiting loop').red.bold}: {e}")
+                logger.info(f"{color('Exiting loop').red.bold}: {e}")
                 exit(0)
             # except Exception as e:
             #     print(f"{type(e).__name__}: {e}")
             #     return
-
-
-# class BasicRecursiveLoop:
-#     def __init__(self, starting_prompt: Prompt) -> None:
-#         self.starting_prompt = starting_prompt
-
-#     def run(self) -> Result:
-#         prompt = self.starting_prompt
-#         while True:
-#             try:
-#                 result = prompt.run()
-#             except ExitLoop as e:
-#                 print(f"{color('Exiting loop').red.bold}: {e}")
-#                 exit(0)
-#             if isinstance(result, Result):
-#                 return result
-#             elif isinstance(result, Prompt):
-#                 prompt = result
-#                 continue
-#             else:
-#                 raise UnexpectedResultType(f"{type(result)}")
