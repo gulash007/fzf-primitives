900599e9a4f102d9ba63dbfc6cfd37397518d939
honza <hai.linh.ng91@gmail.com>
2023-06-22 01:29:02 +0200

    ChromeWindowsManager: Add 'Open windows.json' on_event

--- ChromeWindowManager/__main__.py
+++ ChromeWindowManager/__main__.py
@@ -162,6 +162,7 @@ def close_window(prompt_data: PromptData):
     "source ~/.zshforchrome 2>/dev/null && echo {} | awk '{ print $1 }' | read -r window_id && brotab query -windowId ${window_id:2} | brotab_format_better_line",
 )
 @mods.on_event("ctrl-w").run("Close window", PostProcessAction(close_window), end_prompt="accept")
+@mods.on_event("ctrl-o").run("Open windows.json", ShellCommand(f"code-insiders '{STORED_WINDOWS_PATH}'"))
 def run_window_selection_prompt(prompt_data: PromptData) -> Result:
     return DefaultPrompt.run(prompt_data=prompt_data)
 
