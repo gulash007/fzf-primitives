670aef8c5d0ad2a90850a40129c2eaf0d6eca13b
honza <hai.linh.ng91@gmail.com>
2023-06-30 01:37:55 +0200

    automate.sh: Fix path

--- tools/custom_automator/automate.sh
+++ tools/custom_automator/automate.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 while read -r line; do
-    curl -XPOST "localhost:$1" -d "$line"; sleep 0.25
-done <"/Users/honza/Documents/Projects/PythonPackages/fzf_primitives/experimental/core/FzfPrompt/to_automate.txt"
+    curl -XPOST "localhost:$1" -d "$line"
+    sleep 0.25
+done <"$(dirname "$0")/to_automate.txt"
