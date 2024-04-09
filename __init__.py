720c15a961d42f2e67d775b63fe2b0b8b10e9be7
honza <hai.linh.ng91@gmail.com>
2024-04-09 22:25:56 +0200

    Remove more internal imports from user interface

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 from .main import Prompt
 from .core import BasicLoop
 from .core.monitoring import Logger
-from .core.FzfPrompt.Prompt import PromptData, ShellCommand, ServerCall
+from .core.FzfPrompt.Prompt import PromptData
 
-__all__ = ["Prompt", "BasicLoop", "Logger", "PromptData", "ShellCommand", "ServerCall"]
+__all__ = ["Prompt", "BasicLoop", "Logger", "PromptData"]
