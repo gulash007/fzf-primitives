9ae43935bc2c4fba01db44916051af0073243fe8
honza <hai.linh.ng91@gmail.com>
2024-04-09 22:15:09 +0200

    Add __all__ to __init__.py files

--- __init__.py
+++ __init__.py
@@ -2,3 +2,5 @@ from .main import Prompt
 from .core import BasicLoop
 from .core.monitoring import Logger
 from .core.FzfPrompt.Prompt import PromptData, ShellCommand, ServerCall
+
+__all__ = ["Prompt", "BasicLoop", "Logger", "PromptData", "ShellCommand", "ServerCall"]
--- extra/__init__.py
+++ extra/__init__.py
@@ -1,3 +1,5 @@
 from ..core import BasicLoop
 from ..core.FzfPrompt.exceptions import ExitLoop, ExitRound
 from ..core.monitoring import Logger
+
+__all__ = ["BasicLoop", "ExitLoop", "ExitRound", "Logger"]
