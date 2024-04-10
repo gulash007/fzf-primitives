25762b247c935b6ed49a969543b6235e22ca1cfe
honza <hai.linh.ng91@gmail.com>
2024-04-10 19:08:19 +0200

    Check for selection weirdness

--- core/FzfPrompt/Prompt.py
+++ core/FzfPrompt/Prompt.py
@@ -469,6 +469,8 @@ class PromptEndingAction(ParametrizedAction):
         prompt_data.result.query = query
         prompt_data.result.event = event
         prompt_data.result.end_status = self.end_status
+        if not indices and selections != EMPTY_SELECTIONS:
+            raise RuntimeError(f"Unexpected selections value when no indices: {selections}")
         if indices:
             prompt_data.result.extend([prompt_data.choices[i] for i in indices])
             prompt_data.result.stripped_selections = selections
