a2eb43deffbf1392a19dab5d670c139c48bcf814
honza <hai.linh.ng91@gmail.com>
2024-04-16 13:09:22 +0200

    test: Test expected user interface

---
+++ tests/test_user_interface.py
@@ -0,0 +1,32 @@
+import pytest
+
+from .. import Prompt
+from ..core.FzfPrompt.options import Options
+from ..core.mods import Mod, on_event, preview, BindingConflict
+
+
+def test_mod_return_value_types():
+    prompt = Prompt()
+
+    # test chaining on_events
+    assert type(prompt.mod.on_event("ctrl-a").accept) == on_event
+    assert type(prompt.mod.on_event("ctrl-o").open_files()) == on_event
+
+    # test preview not being chainable
+    assert type(prompt.mod.preview()) == preview
+    assert prompt.mod.preview().basic is None
+
+    # test chaining options
+    assert type(prompt.mod.options) == Options
+    assert type(prompt.mod.options.multiselect) == Options
+    assert type(prompt.mod.options.multiselect.header("")) == Options
+
+    # test mod presets
+    assert type(prompt.mod.default) == Mod
+
+
+def test_checking_for_event_conflicts():
+    prompt = Prompt()
+    with pytest.raises(BindingConflict):
+        prompt.mod.on_event("ctrl-a").accept
+        prompt.mod.on_event("ctrl-a").abort
