gulash007/fzf-primitives

commit 26a98aa38debf9ce4fbb5ed5802611c1c1f474e7

❗ref: actions: Remove action resolutions (on the way for them to become immutable)

core/FzfPrompt/action_menu/parametrized_actions.py
@@ -1,28 +1,21 @@
-from typing import Iterable
-
-from ..options import BaseAction, ParametrizedActionType, ParametrizedOptionString, ShellCommandActionType
+from ..options import BaseAction, ParametrizedActionType, ShellCommandActionType
 
 
-class ParametrizedAction(ParametrizedOptionString):
+class ParametrizedAction:
 
-    def __init__(
-        self, template: str, action_type: ParametrizedActionType, placeholders_to_resolve: Iterable[str] = ()
-    ) -> None:
+    def __init__(self, action_value: str, action_type: ParametrizedActionType) -> None:
+        self.action_value = action_value
         self.action_type: ParametrizedActionType = action_type
-        super().__init__(template, placeholders_to_resolve)
 
     def action_string(self) -> str:
-        return f"{self.action_type}({self.resolved_string()})"
+        return f"{self.action_type}({self.action_value})"
 
 
 class ShellCommand(ParametrizedAction):
-    def __init__(
-        self,
-        command_template: str,
-        action_type: ShellCommandActionType = "execute",
-        placeholders_to_resolve: Iterable[str] = (),
-    ) -> None:
-        super().__init__(command_template, action_type, placeholders_to_resolve)
+    def __init__(self, command: str, command_type: ShellCommandActionType = "execute") -> None:
+        self.command = command
+        self.command_type = command_type
+        super().__init__(command, command_type)
 
 
 # Action can just be a string if you know what you're doing (look in `man fzf` for what can be assigned to '--bind')

core/FzfPrompt/previewer.py
@@ -75,7 +75,7 @@ class PreviewChange(ServerCall):
             prompt_data.previewer.set_current_preview(preview.id)
             logger.trace(f"Changing preview to '{preview.name}'", preview=preview.name)
 
-        super().__init__(change_current_preview, action_type="execute-silent")
+        super().__init__(change_current_preview, command_type="execute-silent")
 
 
 class InvokeCurrentPreview(ServerCall):
@@ -87,7 +87,7 @@ class InvokeCurrentPreview(ServerCall):
             logger.trace(f"Showing preview '{preview.name}'", preview=preview.name)
             return preview.output
 
-        super().__init__(preview_function, f"Show preview of {preview.name}", action_type=action_type)
+        super().__init__(preview_function, f"Show preview of {preview.name}", command_type=action_type)
         # HACK: wanna ServerCall to parse parameters of enclosed function first to create the right template
         self.function = get_current_preview
 
@@ -100,7 +100,7 @@ class StorePreviewOutput(ServerCall):
             logger.trace(f"Showing preview '{preview.name}'", preview=preview.name)
 
         super().__init__(store_preview_output, f"Store preview of {preview.name}", "change-preview")
-        self.template = f'preview_output="$({preview_command})"; echo $preview_output && {self.template}'
+        self.command = f'preview_output="$({preview_command})"; echo $preview_output && {self.command}'
 
 
 class PreviewWindowChange(ParametrizedAction):

core/FzfPrompt/action_menu/__init__.py
@@ -54,8 +54,6 @@ class ActionMenu[T, S]:
                     self.bindings[event] = binding + self.bindings[event]
                 case _:
                     raise ValueError(f"Invalid conflict resolution: {conflict_resolution}")
-        if binding.final_action:
-            binding.final_action.resolve_event(event)
         for action in binding.actions:
             if isinstance(action, ServerCall):
                 logger.debug(f"🤙 Adding server call: {action}")

core/FzfPrompt/options/ParametrizedOptionString.py
@@ -1,44 +1,0 @@
-from string import Template
-from typing import Any, Iterable, TypeVar
-
-T = TypeVar("T")
-
-
-class ParametrizedOptionString:
-    def __init__(self, template: str, placeholders_to_resolve: Iterable[str] = ()) -> None:
-        """
-        Args:
-            template (str): Should contain placeholders in the format: $some_placeholder (same as bash variable)
-            placeholders_to_resolve (Iterable[str], optional): An iterable of placeholders that are required
-            to be resolved before this object is used in a fzf option.
-            Defaults to ().
-        """
-        self.template = template
-        self.placeholders_to_resolve = set(placeholders_to_resolve)
-        self.resolved_placeholders: dict[str, Any] = {}
-
-    @property
-    def resolved(self) -> bool:
-        return set(self.resolved_placeholders) == self.placeholders_to_resolve
-
-    def resolve(self, **mapping):
-        for key, value in mapping.items():
-            if key not in self.placeholders_to_resolve:
-                raise RuntimeError(f"{self}: {key} not in {self.placeholders_to_resolve=}")
-            if key in self.resolved_placeholders:
-                raise RuntimeError(f"{self}: {key} already resolved")
-            self.resolved_placeholders.update({key: value})
-
-    def resolved_string(self) -> str:
-        """To resolve into action string that can be used in --bind '<event>:<action string>'"""
-        if not self.resolved:
-            raise NotResolved(
-                f"{self}: Not resolved {self.placeholders_to_resolve.difference(self.resolved_placeholders)}"
-            )
-        return Template(self.template).safe_substitute(self.resolved_placeholders)
-
-    def __str__(self) -> str:
-        return f"{self.__class__.__name__} ({self.placeholders_to_resolve=}, {self.resolved_placeholders=})"
-
-
-class NotResolved(RuntimeError): ...

core/FzfPrompt/options/__init__.py
@@ -5,7 +5,6 @@ from typing import Self
 
 from .actions import BaseAction, ParametrizedActionType, ShellCommandActionType
 from .events import Hotkey, Situation
-from .ParametrizedOptionString import ParametrizedOptionString
 from .values import Border, EndStatus, Layout, Position, RelativeWindowSize
 
 __all__ = [
@@ -20,7 +19,6 @@ __all__ = [
     "RelativeWindowSize",
     "Layout",
     "Border",
-    "ParametrizedOptionString",
 ]
 
 DEFAULT_OPTS = [

core/FzfPrompt/prompt_data.py
@@ -160,4 +160,4 @@ class ReloadChoices(ServerCall):
             prompt_data.presented_choices = lines
             return "\n".join(lines)
 
-        super().__init__(reload_choices, action_type="reload")
+        super().__init__(reload_choices, command_type="reload")

core/FzfPrompt/server.py
@@ -37,14 +37,14 @@ class ServerCall[T, S](ShellCommand):
         self,
         function: ServerCallFunction[T, S],
         custom_name: str | None = None,
-        action_type: ShellCommandActionType = "execute",
+        command_type: ShellCommandActionType = "execute",
     ) -> None:
         self.function = function
         self.name = custom_name or function.__name__
         self.id = f"{self.name} ({id(self)})"
 
-        template, placeholders_to_resolve = Request.create_template(self.id, function, action_type)
-        super().__init__(template, action_type, placeholders_to_resolve)
+        command = Request.create_command(self.id, function, command_type)
+        super().__init__(command, command_type)
 
     def __str__(self) -> str:
         return f"{self.id}: {super().__str__()}"
@@ -54,33 +54,23 @@ type PostProcessor[T, S] = Callable[[PromptData[T, S]], Any]
 
 
 class PromptEndingAction[T, S](ServerCall):
-    def __init__(self, end_status: EndStatus, post_processor: PostProcessor[T, S] | None = None) -> None:
+    def __init__(
+        self, end_status: EndStatus, event: Hotkey | Situation, post_processor: PostProcessor[T, S] | None = None
+    ) -> None:
         self.end_status: EndStatus = end_status
         self.post_processor = post_processor
-        self._event: Hotkey | Situation | None = None
+        self.event: Hotkey | Situation = event
         # ❗ Needs to be silent, otherwise program can get stuck when waiting for user input on error in Server
-        super().__init__(self.pipe_results, action_type="execute-silent")
+        super().__init__(self.pipe_results, command_type="execute-silent")
 
-    @property
-    def event(self) -> Hotkey | Situation:
-        if self._event is None:
-            raise RuntimeError("event not resolved")
-        return self._event
-
-    def pipe_results(self, prompt_data: PromptData[T, S], event: Hotkey | Situation):
-        prompt_data.finish(event, self.end_status)
+    def pipe_results(self, prompt_data: PromptData[T, S]):
+        prompt_data.finish(self.event, self.end_status)
         if self.post_processor:
             self.post_processor(prompt_data)
         logger.debug(f"Piping results:\n{prompt_data.result}")
 
-    @single_use_method
-    def resolve_event(self, event: Hotkey | Situation):
-        self._event = event
-        self.resolve(event=event)
-
     def __str__(self) -> str:
-        event = self.event if self._event else "<event not resolved>"
-        return f"{event}: end status '{self.end_status}' with {self.post_processor} post-processor: {super().__str__()}"
+        return f"{self.event}: end status '{self.end_status}' with {self.post_processor} post-processor: {super().__str__()}"
 
 
 class Request(pydantic.BaseModel):
@@ -90,23 +80,19 @@ class Request(pydantic.BaseModel):
     kwargs: dict = {}
 
     @staticmethod
-    def create_template(
-        server_call_id: str, function: ServerCallFunction, command_type: ShellCommandActionType
-    ) -> tuple[str, list[str]]:
+    def create_command(server_call_id: str, function: ServerCallFunction, command_type: ShellCommandActionType) -> str:
 
         parameters = list(inspect.signature(function).parameters.values())[1:]  # excludes prompt_data
         jq_args = []
-        placeholders_to_resolve = []
         for parameter in parameters:
             if isinstance(parameter.default, CommandOutput):
+                # HACK: when default value of a parameter of ServerCallFunction is of type CommandOutput
+                # then the parameter is going to be injected with the output of the value executed as shell command
                 jq_args.append(f'--arg {parameter.name} "$({parameter.default})"')
             else:
-                # to be replaced using .resolve or is an environment variable
+                # otherwise it's going to be injected with a shell variable of the same name (mainly env vars)
                 jq_args.append(f'--arg {parameter.name} "${parameter.name}"')
-                # environmental variables are recognized by being all uppercase
-                if not re.match("^[A-Z_]*$", parameter.name):
-                    placeholders_to_resolve.append(parameter.name)
-        template = (
+        return (
             PromptState.create_command()
             + " | jq --compact-output '{prompt_state:.} + {"
             + f'server_call_name:"{server_call_id}",command_type:"{command_type}"'
@@ -114,7 +100,6 @@ class Request(pydantic.BaseModel):
             + " ".join(jq_args)
             + " | nc localhost $SOCKET_NUMBER"
         )
-        return template, placeholders_to_resolve
 
 
 PLACEHOLDERS = {

core/mods/on_event.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-import functools
 from pathlib import Path
 from typing import Callable, Literal, Self
 
@@ -39,34 +38,38 @@ FILE_EDITORS: dict[FileEditor, str] = {"VS Code": "code", "VS Code - Insiders": 
 
 class OnEvent[T, S]:
     def __init__(self, *events: Hotkey | Situation, conflict_resolution: ConflictResolution = "raise error"):
-        self._bindings: list[Binding] = []
+        self._bindings: dict[Hotkey | Situation, Binding] = {}
         self._events: list[Hotkey | Situation] = list(events)
         self._conflict_resolution: ConflictResolution = conflict_resolution
 
     def run(self, name: str, *actions: Action) -> Self:
-        self._bindings.append(Binding(name, *actions))
+        for event in self._events:
+            self._add_binding(event, name, *actions)
         return self
 
+    def _add_binding(self, event: Hotkey | Situation, name: str, *actions: Action):
+        if event not in self._bindings:
+            self._bindings[event] = Binding(name, *actions)
+        else:
+            self._bindings[event] += Binding(name, *actions)
+
     def run_function(self, name: str, function: ServerCallFunction[T, S], *base_actions: BaseAction) -> Self:
-        return self.run(name, ServerCall(function, action_type="execute"), *base_actions)
+        return self.run(name, ServerCall(function, command_type="execute"), *base_actions)
 
     def run_shell_command(
         self, name: str, command: str, *base_actions: BaseAction, command_type: ShellCommandActionType = "execute"
     ) -> Self:
-        return self.run(name, ShellCommand(command, action_type=command_type), *base_actions)
+        return self.run(name, ShellCommand(command, command_type=command_type), *base_actions)
 
     def end_prompt(
         self, name: str, end_status: EndStatus, post_processor: Callable[[PromptData[T, S]], None] | None = None
     ):
-        self.run(name, PromptEndingAction(end_status, post_processor))
+        for event in self._events:
+            self._add_binding(event, name, PromptEndingAction(end_status, event, post_processor))
 
     def __call__(self, prompt_data: PromptData[T, S]) -> None:
-        for event in self._events:
-            prompt_data.action_menu.add(
-                event,
-                functools.reduce(lambda b1, b2: b1 + b2, self._bindings),
-                conflict_resolution=self._conflict_resolution,
-            )
+        for event, binding in self._bindings.items():
+            prompt_data.action_menu.add(event, binding, conflict_resolution=self._conflict_resolution)
 
     # presets
     @property

commit 7d55e4df6efd9ad6c124dbd23117df8342bae67a

fix+test: Preview: Fix the hacky way of showing and storing preview output of shell command
Error introduced in 26a98aa38debf9ce4fbb5ed5802611c1c1f474e7

core/FzfPrompt/action_menu/parametrized_actions.py
@@ -13,9 +13,16 @@ class ParametrizedAction:
 
 class ShellCommand(ParametrizedAction):
     def __init__(self, command: str, command_type: ShellCommandActionType = "execute") -> None:
-        self.command = command
-        self.command_type = command_type
+        self.action_type: ShellCommandActionType
         super().__init__(command, command_type)
+
+    @property
+    def command(self) -> str:
+        return self.action_value
+
+    @property
+    def command_type(self) -> ShellCommandActionType:
+        return self.action_type
 
 
 # Action can just be a string if you know what you're doing (look in `man fzf` for what can be assigned to '--bind')

core/FzfPrompt/previewer.py
@@ -100,7 +100,8 @@ class StorePreviewOutput(ServerCall):
             logger.trace(f"Showing preview '{preview.name}'", preview=preview.name)
 
         super().__init__(store_preview_output, f"Store preview of {preview.name}", "change-preview")
-        self.command = f'preview_output="$({preview_command})"; echo $preview_output && {self.command}'
+        # HACK ❗
+        self.action_value = f'preview_output="$({preview_command})"; echo $preview_output && {self.command}'
 
 
 class PreviewWindowChange(ParametrizedAction):

tests/TestPrompt.py
@@ -46,7 +46,7 @@ def prompt_builder():
     prompt.mod.on_hotkey().CTRL_N.run_function("wait", wait_for_input)
     # prompt.mod.on_hotkey().CTRL_X.run_function("wait", bad_server_call_function) # uncomment to reveal error
     prompt.mod.preview("ctrl-y").basic
-    prompt.mod.preview("ctrl-6", "50%", "up").custom(name="basic2", command="echo {}", store_output=False)
+    prompt.mod.preview("ctrl-6", "50%", "up").custom(name="basic2", command="echo {}", store_output=True)
     return prompt
 
 

tests/test_preview.py
@@ -36,6 +36,16 @@ def test_no_explicit_main_preview_having_first_added_as_main():
     assert prompt.obj[1:] == ["first"]
 
 
+def test_storing_preview_output():
+    prompt = Prompt(obj="")
+    command = "printf 'hello\\nworld'"
+    prompt.mod.preview().custom("first", command)
+
+    prompt.mod.automate(config.DEFAULT_ACCEPT_HOTKEY)
+    prompt.run()
+    assert prompt.current_preview == "hello\nworld"
+
+
 # Modding
 def get_file_preview_prompt():
     prompt = Prompt([__file__], obj=[])