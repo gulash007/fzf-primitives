635c4fc72ade3395e9b3f10e51a697b332f046f8
honza <hai.linh.ng91@gmail.com>
2024-04-27 18:24:26 +0200

    log: ParametrizedAction: Improve readability of __str__

--- core/FzfPrompt/action_menu/parametrized_actions.py
+++ core/FzfPrompt/action_menu/parametrized_actions.py
@@ -10,6 +10,9 @@ class ParametrizedAction:
     def action_string(self) -> str:
         return f"{self.action_type}({self.action_value})"
 
+    def __str__(self) -> str:
+        return f"{self.__class__.__name__}: {self.action_type}({self.action_value if len(self.action_value) < 20 else f'{self.action_value[:20]}...'})"
+
 
 class ShellCommand(ParametrizedAction):
     def __init__(self, command: str, command_type: ShellCommandActionType = "execute") -> None:
