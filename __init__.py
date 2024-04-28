ec0d9cb07175cf4ac58b34d422b3c143ae7988c2
honza <hai.linh.ng91@gmail.com>
2024-04-28 17:31:46 +0200

    fix: Expose Result from root package

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 from .core import Prompt
-from .core.FzfPrompt import Preview, PromptData
+from .core.FzfPrompt import Preview, PromptData, Result
 from .core.monitoring import Logger
 from .extra import BasicLoop
 
-__all__ = ["Prompt", "BasicLoop", "Logger", "PromptData", "Preview"]
+__all__ = ["Prompt", "BasicLoop", "Logger", "PromptData", "Preview", "Result"]
