80f1508377d104343f2a472616abdf5acc9491b7
honza <hai.linh.ng91@gmail.com>
2024-04-30 16:48:26 +0200

    Remove LoggingComponent from extra

--- extra/__init__.py
+++ extra/__init__.py
@@ -1,4 +1,3 @@
 from ..core.FzfPrompt.exceptions import Quitting, Aborted
-from ..core.monitoring import LoggedComponent
 
-__all__ = ["Quitting", "Aborted", "LoggedComponent"]
+__all__ = ["Quitting", "Aborted"]
