4ee78d985386880395873e7cadc9401f1d5ca35f
honza <hai.linh.ng91@gmail.com>
2024-05-04 00:45:59 +0200

    log: Disable logging by default

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ from .core.FzfPrompt.options.events import Hotkey
 
 
 class Config:
-    logging_enabled: bool = True
+    logging_enabled: bool = False
     use_basic_hotkeys: bool = True
     default_accept_hotkey: Hotkey = "enter"
     default_abort_hotkey: Hotkey = "esc"
