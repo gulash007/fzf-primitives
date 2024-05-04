36a10697821b5364a992065b5858013920134c2d
honza <hai.linh.ng91@gmail.com>
2024-05-04 23:19:09 +0200

    log: Transformation: Improve logging

--- core/FzfPrompt/action_menu/transformation.py
+++ core/FzfPrompt/action_menu/transformation.py
@@ -13,13 +13,14 @@ type TransformationFunction[T, S] = Callable[[PromptData[T, S]], Iterable[Action
 
 
 class Transformation[T, S](ServerCall[T, S], LoggedComponent):
-    def __init__(self, transformation_function: TransformationFunction[T, S], custom_name: str | None = None) -> None:
+    def __init__(self, transformation_function: TransformationFunction[T, S]) -> None:
         LoggedComponent.__init__(self)
+        name = f"Transformation ({transformation_function.__name__})"
 
         def get_transformation_string(prompt_data: PromptData[T, S]) -> str:
-            self.logger.debug(f"Transformation: {self.name}")
-            binding = Binding(f"Created by Transformation ({self.name})", *transformation_function(prompt_data))
+            self.logger.debug(name)
+            binding = Binding(f"Created by {name}", *transformation_function(prompt_data))
             prompt_data.action_menu.add_server_calls(binding)
             return binding.to_action_string()
 
-        super().__init__(get_transformation_string, custom_name, "transform")
+        super().__init__(get_transformation_string, name, "transform")
