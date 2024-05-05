01b8150608b55b1e6f27b2d90820d305d5c3758b
honza <hai.linh.ng91@gmail.com>
2024-05-05 21:30:20 +0200

    feat: Expose MultiDimensionalGenerator in root package

--- __init__.py
+++ __init__.py
@@ -1,7 +1,18 @@
 from .core import Prompt
 from .core.FzfPrompt import Preview, PromptData, Result
 from .core.monitoring import LoggedComponent, Logger
+from .core.mods import MultiDimensionalGenerator
 from .extra import BasicLoop
 from . import actions
 
-__all__ = ["Prompt", "BasicLoop", "LoggedComponent", "Logger", "PromptData", "Preview", "Result", "actions"]
+__all__ = [
+    "Prompt",
+    "BasicLoop",
+    "LoggedComponent",
+    "Logger",
+    "PromptData",
+    "Preview",
+    "Result",
+    "actions",
+    "MultiDimensionalGenerator",
+]
--- core/mods/__init__.py
+++ core/mods/__init__.py
@@ -2,5 +2,6 @@ from .Mod import Mod
 from .on_event import OnEvent
 from .post_processing import PostProcessing
 from .preview_mod import PreviewMod
+from .multi_dimensional_generator import MultiDimensionalGenerator
 
-__all__ = ["Mod", "OnEvent", "PostProcessing", "PreviewMod"]
+__all__ = ["Mod", "OnEvent", "PostProcessing", "PreviewMod", "MultiDimensionalGenerator"]
