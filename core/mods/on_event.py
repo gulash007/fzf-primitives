c1c7361f4ce7e7264f3d919ffd55be1c28e1a25c
honza <hai.linh.ng91@gmail.com>
2024-05-08 15:31:21 +0200

    feat: mods: Add OnEvent.auto_repeat_run and use it to implement reloading choices repeatedly

--- core/mods/on_event.py
+++ core/mods/on_event.py
@@ -1,14 +1,19 @@
 from __future__ import annotations
 
-from abc import ABC, abstractmethod
+import shlex
 import subprocess
+from abc import ABC, abstractmethod
 from pathlib import Path
+from threading import Thread
 from typing import Callable, Literal, Self
 
 import pyperclip
 
+from fzf_primitives.core.monitoring import LoggedComponent
+
 from ..FzfPrompt import (
     Action,
+    ActionsBuilder,
     Binding,
     ChoicesGetter,
     ConflictResolution,
@@ -20,30 +25,10 @@ from ..FzfPrompt import (
     ServerCallFunction,
     ShellCommand,
     Transform,
-    ActionsBuilder,
 )
 from ..FzfPrompt.constants import SHELL_COMMAND
 from ..FzfPrompt.options.actions import BaseAction, ShellCommandActionType
 from ..FzfPrompt.options.events import Hotkey, Situation
-
-
-def clip_current_preview(prompt_data: PromptData):
-    pyperclip.copy(prompt_data.get_current_preview())
-
-
-def clip_options(prompt_data: PromptData):
-    pyperclip.copy(str(prompt_data.options))
-
-
-type FileEditor = Literal["VS Code", "VS Code - Insiders", "Vi", "Vim", "NeoVim", "Nano"]
-FILE_EDITORS: dict[FileEditor, str] = {
-    "VS Code": "code",
-    "VS Code - Insiders": "code-insiders",
-    "Vi": "vi",
-    "Vim": "vim",
-    "NeoVim": "nvim",
-    "Nano": "nano",
-}
 
 
 class OnEventBase[T, S](ABC):
@@ -86,8 +71,40 @@ class OnEvent[T, S](OnEventBase[T, S]):
     def run_transform(self, name: str, get_actions: ActionsBuilder[T, S], *base_actions: BaseAction) -> Self:
         return self.run(name, Transform(get_actions), *base_actions)
 
-    def reload_choices(self, choices_getter: ChoicesGetter[T, S], *, sync: bool = False):
-        return self.run(f"reload choices{' (sync)' if sync else ''}", ReloadChoices(choices_getter, sync=sync))
+    def reload_choices(
+        self, choices_getter: ChoicesGetter[T, S], *, sync: bool = False, repeat_interval: float | None = None
+    ):
+        name = f"reload choices{' (sync)' if sync else ''}"
+        if repeat_interval is None:
+            return self.run(name, ReloadChoices(choices_getter, sync=sync))
+        if repeat_interval < 0.2:
+            raise ValueError("repeat_interval must be at least 0.2")
+        return self.auto_repeat_run(name, ReloadChoices(choices_getter, sync=sync), repeat_interval=repeat_interval)
+
+    def auto_repeat_run(self, name: str, *actions: Action, repeat_interval: float = 0.5) -> Self:
+        thread = AutomatingThread(*actions, repeat_interval=repeat_interval)
+
+        def repeat_http_action_request(prompt_data: PromptData, FZF_PORT: str):
+            nonlocal thread
+            prompt_data.action_menu.add_server_calls(Binding("", *actions))
+            thread.set_port(FZF_PORT)
+            if not thread.is_running:
+                subprocess.Popen(
+                    shlex.join(["curl", "-XPOST", f"localhost:{FZF_PORT}", "-d", "change-prompt(Auto-updating...> )"]),
+                    shell=True,
+                )
+                thread.start()
+                thread.is_running = True
+            else:
+                # TODO: reset prompt back to previous
+                subprocess.Popen(
+                    shlex.join(["curl", "-XPOST", f"localhost:{FZF_PORT}", "-d", "change-prompt(> )"]),
+                    shell=True,
+                )
+                thread.should_stop = True
+                thread = AutomatingThread(*actions, repeat_interval=repeat_interval)
+
+        return self.run_function(f"Every {repeat_interval:.2f}s {name}", repeat_http_action_request)
 
     def end_prompt(
         self, name: str, end_status: EndStatus, post_processor: Callable[[PromptData[T, S]], None] | None = None
@@ -168,3 +185,62 @@ class OnEvent[T, S](OnEventBase[T, S]):
         file_getter = file_getter or (lambda pd: pd.current_state.lines)
         command = FILE_EDITORS[app]
         return self.run_function(f"open files in {app}", lambda pd: subprocess.run([command, "--", *file_getter(pd)]))
+
+
+def clip_current_preview(prompt_data: PromptData):
+    pyperclip.copy(prompt_data.get_current_preview())
+
+
+def clip_options(prompt_data: PromptData):
+    pyperclip.copy(str(prompt_data.options))
+
+
+type FileEditor = Literal["VS Code", "VS Code - Insiders", "Vi", "Vim", "NeoVim", "Nano"]
+FILE_EDITORS: dict[FileEditor, str] = {
+    "VS Code": "code",
+    "VS Code - Insiders": "code-insiders",
+    "Vi": "vi",
+    "Vim": "vim",
+    "NeoVim": "nvim",
+    "Nano": "nano",
+}
+
+
+class AutomatingThread(Thread, LoggedComponent):
+    def __init__(self, *actions: Action, repeat_interval: float = 0.5) -> None:
+        super().__init__(daemon=True)
+        LoggedComponent.__init__(self)
+        self.is_running = False
+        self.should_stop = False
+        self.actions = actions
+        self.repeat_interval = repeat_interval
+        self.port: str
+
+    def run(self) -> None:
+        import subprocess
+        import time
+
+        while True:
+            try:
+                subprocess.Popen(
+                    shlex.join(
+                        [
+                            "curl",
+                            "-XPOST",
+                            f"localhost:{self.port}",
+                            "-d",
+                            Binding("", *self.actions).to_action_string(),
+                        ]
+                    ),
+                    shell=True,  # ❗ required for getting FZF_PORT
+                )
+                if self.should_stop:
+                    break
+            except Exception as err:
+                self.logger.exception(err)
+                continue
+            finally:
+                time.sleep(self.repeat_interval)
+
+    def set_port(self, port: str):
+        self.port = port
