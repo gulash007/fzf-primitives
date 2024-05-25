775d5e9ba8941e297040f0f204616bb93e907011
honza <hai.linh.ng91@gmail.com>
2024-05-25 14:00:01 +0200

    fix: mods: ReloadChoices: Don't actually reload choices on error
    Previously would still send a error string response to 'reload-choices'
    action which would attempt to reload choices using that string even if
    an error occurred.

--- core/mods/on_event/presets/actions.py
+++ core/mods/on_event/presets/actions.py
@@ -2,26 +2,28 @@ from __future__ import annotations
 
 from typing import Callable
 
-from ....FzfPrompt import ChoicesAndLinesMismatch, PreviewFunction, PromptData, ServerCall
+from ....FzfPrompt import PreviewFunction, PromptData, ServerCall
+from ....monitoring import LoggedComponent
 
 type ChoicesGetter[T, S] = Callable[[PromptData[T, S]], tuple[list[T], list[str] | None]]
 
 
-class ReloadChoices[T, S](ServerCall[T, S]):
+class ReloadChoices[T, S](ServerCall[T, S], LoggedComponent):
     def __init__(self, choices_getter: ChoicesGetter[T, S], *, sync: bool = False):
+        LoggedComponent.__init__(self)
+
         def reload_choices(prompt_data: PromptData[T, S]):
-            choices, lines = choices_getter(prompt_data)
-            if lines is None:
-                lines = [str(choice) for choice in choices]
-            else:
-                try:
-                    prompt_data.check_choices_and_lines_length(choices, lines)
-                except ChoicesAndLinesMismatch as err:
-                    input(str(err))
-                    raise
-            prompt_data.choices = choices
-            prompt_data.presented_choices = lines
-            return "\n".join(lines)
+            try:
+                choices, lines = choices_getter(prompt_data)
+                if lines is None:
+                    lines = [str(choice) for choice in choices]
+                prompt_data.check_choices_and_lines_length(choices, lines)
+                prompt_data.choices = choices
+                prompt_data.presented_choices = lines
+                return "\n".join(lines)
+            except Exception as e:
+                self.logger.error(f"Error in reload_choices: {e}")
+                return None
 
         super().__init__(reload_choices, command_type="reload-sync" if sync else "reload")
 
