5616b040551c7e75f79dcd3e31318128c90c0560
honza <hai.linh.ng91@gmail.com>
2025-09-13 00:46:28 +0200

    feat+docs: Improve generating fzf manual, add file for fzf env vars

--- tools/generate_from_fzf_manual/get_man_fzf.sh
+++ tools/generate_from_fzf_manual/get_man_fzf.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+output_file="$script_dir/man_fzf.txt"
 
-echo "Generating fzf manual for version $(fzf --version)..."
-COLUMNS=120 man fzf | col -b > "fzf-manual.txt"
+echo "Generating fzf manual for version $(fzf --version) into $output_file"
+MANWIDTH=140 man fzf | col -b > "$output_file"
