f3faf9d56eb453b1797a43bc69d2399a8491a4cb
honza <hai.linh.ng91@gmail.com>
2025-09-14 00:11:25 +0200

    feat: config: Add env var for enabling logging

--- src/fzf_primitives/config.py
+++ src/fzf_primitives/config.py
@@ -1,8 +1,12 @@
+import os
+
 from .core.FzfPrompt.options.events import Hotkey
+
+ENV_VAR_FOR_LOGGING = "FZF_PRIMITIVES_ENABLE_INTERNAL_LOGGING"
 
 
 class Config:
-    logging_enabled: bool = False
+    logging_enabled: bool = os.getenv(ENV_VAR_FOR_LOGGING, "0") == "1"
     use_basic_hotkeys: bool = True
     default_accept_hotkey: Hotkey = "enter"
     default_abort_hotkey: Hotkey = "esc"
