1c13a2936ccffd1832c4c039c0da2199fdf362a2
honza <hai.linh.ng91@gmail.com>
2025-09-29 21:21:31 +0200

    test: Add test for main imports

---
+++ tests/test_imports.py
@@ -0,0 +1,9 @@
+from fzf_primitives import *
+from fzf_primitives.actions import *
+from fzf_primitives.config import Config
+from fzf_primitives.extra import *
+from fzf_primitives.utils import *
+
+
+def test_imports():
+    pass  # If the imports fail, this test will fail
